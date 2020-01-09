1a1dde41e357c1fa4fd7cd7f9235985981aa02f0
xuzhen <xuzhen@koolearn.com>
2020-01-09 14:08:41 +0800

    设置首页

--- shop-web/src/main/java/com/xz/shop/controller/ShopCenterController.java
+++ shop-web/src/main/java/com/xz/shop/controller/ShopCenterController.java
@@ -1,5 +1,7 @@
 package com.xz.shop.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/shop")
 @PropertySource(value = "application.properties",encoding = "utf-8")
 public class ShopCenterController {
+private static final Logger log= LoggerFactory.getLogger(ShopCenterController.class);
 
 
     @RequestMapping(value = "/login",method={RequestMethod.GET,RequestMethod.POST})
     public String login(){
+        log.info("日志测试xz###############");
         return "xz";
     }
 
